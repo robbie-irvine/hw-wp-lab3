f992b957ef1044a19ec396273e272dac1fe7ba91
robbie-irvine <90905592+robbie-irvine@users.noreply.github.com>
2021-11-19 08:30:13 +0000

    Implemeted db connection

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
 });
 
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+
+//route for contacts
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//route for contacts
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//pass requests to the router middleware
+const router = require("./apis/routes");
+app.use(router);
+
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
